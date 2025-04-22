cba714956e2cd1b99d61a58b5da05caf7f7a852b
Olga-Leonteva <panakushinaoa@gmail.com>
2025-04-22 23:00:12 +0300

    commit_01

--- Leonteva_Diplom_Kinopoisk/API/test_api.py
+++ Leonteva_Diplom_Kinopoisk/API/test_api.py
@@ -11,7 +11,7 @@ def test_seach_film_by_title():
     api = Kinopoisk_API()
     new_title = "The Beekeeper"
     response = api.seach_film_by_title(new_title)
-    assert response.status_code == 200, "Баг"
+    assert response.status_code == 200
     assert response.json()["docs"][0]["alternativeName"] == new_title
 
 
@@ -22,7 +22,7 @@ def test_seach_name_person():
     name_person = "Johnny Depp"
     response = api.seach_name_person(name_person)
     assert response.json()["docs"][0]["enName"] == name_person
-    assert response.status_code == 200, "Баг"
+    assert response.status_code == 200
 
 
 @allure.title("Поиск фильма по id актера")
@@ -33,7 +33,7 @@ def test_seach_film_person_id():
     response = api.seach_film_person_id(person_id)
     print(response)
     assert response.json()["docs"][0]["id"] == 6461896
-    assert response.status_code == 200, "Баг"
+    assert response.status_code == 200
 
 
 @allure.feature("Негативные проверки")
@@ -43,7 +43,7 @@ def test_seach_wrong_id():
     api = Kinopoisk_API()
     wrong_id = "249"
     response = api.seach_wrong_id(wrong_id)
-    assert response.status_code == 400, "Баг"
+    assert response.status_code == 400
     assert response.json()["error"] == "Bad Request"
 
 
@@ -53,5 +53,5 @@ def test_seach_wrong_category():
     api = Kinopoisk_API()
     wrong_category = "небылица"
     response = api.seach_wrong_category(wrong_category)
-    assert response.status_code == 200, "Баг"
+    assert response.status_code == 200
     assert response.json()["total"] == 0
